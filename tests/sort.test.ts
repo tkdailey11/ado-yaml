7349011117a3698962da446a3ca9a30b92373611
Tyler Dailey <tkdailey11@gmail.com>
2023-03-06 21:48:20 -0700

    Add more sort unit tests

--- tests/sort.test.ts
+++ tests/sort.test.ts
@@ -21,3 +21,42 @@ test("basic string sort - descending", () => {
 test("integer sort - ascending", () => {
   expect(adoSort.sortArray([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4])
 })
+
+test("object array sort - ascending", () => {
+  expect(
+    adoSort.sortArray([
+      { name: "a", value: "2" },
+      { value: "1", name: "c" },
+      { other: "1", name: "b" },
+      { type: "1", name: "d" },
+    ])
+  ).toEqual([
+    { name: "a", value: "2" },
+    { other: "1", name: "b" },
+    { value: "1", name: "c" },
+    { type: "1", name: "d" },
+  ])
+})
+
+test("object array sort - combined", () => {
+  expect(
+    adoSort.sortArray(
+      [
+        { name: "a", value: "2" },
+        { value: "1", name: "c" },
+        { other: "1", name: "b" },
+        { type: "1", name: "d" },
+        "def",
+        "abc",
+      ],
+      false
+    )
+  ).toEqual([
+    { type: "1", name: "d" },
+    { value: "1", name: "c" },
+    { other: "1", name: "b" },
+    { name: "a", value: "2" },
+    "def",
+    "abc",
+  ])
+})
